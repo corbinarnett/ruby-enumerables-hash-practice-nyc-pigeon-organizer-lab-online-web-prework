fddf6be57dc2c1c38b718d62efff1e47f5d39d1c
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-25 04:21:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # create empty hash
+  organized_hash = {}
+
+
+  data.each do |traits, traits_hash|    # search data for traits (color, gender, lives)
+
+    traits_hash.each do |details, details_array| # search traits_hash for details e.q. purple, mail, subway
+
+      details_array.each do |name| # search details_array for names :purple => [>>>"Theo", "Peter Jr.", "Lucky"<<<]
+
+        # if organized_hash[name] doesn't exist, create empty hash
+        if !organized_hash[name]
+          organized_hash[name] = {}
+        end
+
+        # if organized_hash[name][traits] doesn't exist, create empty array
+        if !organized_hash[name][traits]
+          organized_hash[name][traits] = []
+        end
+
+
+        organized_hash[name][traits] << details.to_s # push details as strings into traits array
+
+      end
+    end
+  end
+
+  organized_hash
 end
